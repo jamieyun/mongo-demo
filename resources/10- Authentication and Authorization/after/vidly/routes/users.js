6202833da42521c515b94de9efd4a817880b0edd
jamie <jamie@cntt.co.kr>
2020-07-13 13:41:57 +0900

    user model and users server 추가

--- resources/10- Authentication and Authorization/after/vidly/routes/users.js
+++ resources/10- Authentication and Authorization/after/vidly/routes/users.js
@@ -17,6 +17,17 @@ router.post('/', async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
+  // let user = await User.findOne({email: req.body.email})
+  // if(user) return res.status(400).send('User already registered.')
+
+  // user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password
+  // })
+  // await user.save();
+
+  // res.send(user);
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
 
