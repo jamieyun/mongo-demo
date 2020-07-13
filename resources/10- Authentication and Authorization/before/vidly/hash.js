21e768d3d63867339c18064f4e12dcb25f3c29ee
jamie <jamie@cntt.co.kr>
2020-07-13 13:47:56 +0900

    bcrypt hash password and salt code 추가

---
+++ resources/10- Authentication and Authorization/before/vidly/hash.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcrypt')
+
+async function run(){
+    const salt = await bcrypt.genSalt(10)
+    const hased = await bcrypt.hash('1234',salt)
+    console.log(salt)
+    console.log(hased)
+}
+run();
--- resources/10- Authentication and Authorization/before/vidly/routes/users.js
+++ resources/10- Authentication and Authorization/before/vidly/routes/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const _ = require('lodash')
 const {User, validate} = require('../models/user');
 const mongoose = require('mongoose');
@@ -14,6 +15,9 @@ router.post('/', async (req, res) => {
     user = new User(
         _.pick(req.body, ['name','email','password'])
     )
+    
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
 
     await user.save();
 
